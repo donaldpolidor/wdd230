cfa2f4f6e31a1a6b529106850010e2b5838b9a1f
do-3 <donaldpolidor30@github.com>
2025-01-09 16:07:25 -0500

    first commit

---
+++ week1/scripts/getDates.js
@@ -0,0 +1,6 @@
+// Set the current year dynamically
+document.getElementById("year").textContent = new Date().getFullYear();
+
+// Set the last modified date
+document.getElementById("lastModified").textContent = "Last Updated: " + document.lastModified;
+
