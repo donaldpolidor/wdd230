62a9750a8626f1ede0e238d074819302d8463a9b
do-3 <donaldpolidor30@github.com>
2025-01-20 07:48:13 -0500

    commit message

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -1,9 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
     const hamburger = document.querySelector(".hamburger");
     const menu = document.querySelector("nav ul");
+    const heroImage = document.querySelector("main img");
 
     hamburger.addEventListener("click", () => {
         menu.classList.toggle("show");
+
+        if (menu.classList.contains("show")) {
+            heroImage.style.marginTop = menu.offsetHeight + "px";
+        } else {
+            heroImage.style.marginTop = "1rem";
+        }
     });
 
     // Dynamically update the year and last modified date
